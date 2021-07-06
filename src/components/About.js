c09a67c53e1eea7a222c383f7ed0281c3b2b64c9
VedranSelak <selakvedran@gmail.com>
2021-07-06 22:34:05 +0200

    added about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,39 @@
 import '../css/about.scss'
+import { FaAngleDown } from 'react-icons/fa'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
+
 
 const About = () => {
+
+    const paragraf = useRef();
+
+    useEffect(() => {
+        init(paragraf.current, {
+            showCursor: true,
+            loop: false,
+            typeSpeed:  50,
+            startDelay: 0,
+            strings: ["I am a, hardworking, second year student at the International Burch University. I love developing web applications and websites. I am always eager to learn new skills and improve as a developer."]
+        })
+    }, [])
+
     return (
         <div id='about' className='about'>
-            
+            <div className="left">
+                <div className="container-img">
+                    <img src="images/profil.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi, I am Vedran Selak</h2>
+                    <p><span ref={paragraf}></span></p>
+                </div>
+                <div className='down-icon' href="#portfolio">
+                    <a href="#portfolio"><FaAngleDown /></a>
+                </div>
+            </div>
         </div>
     )
 }
