026f3d511f794ffba57251a57074bb48c855161c
VedranSelak <selakvedran@gmail.com>
2021-07-08 15:59:16 +0200

    added contact me ui

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,9 +1,33 @@
 import '../css/contactme.scss'
+import{ init } from 'emailjs-com';
+init("");
 
 const ContactMe = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(process.env)
+
+        // emailjs.sendForm(`gmail`, apiKey.TEMPLATE_ID, e.target, apiKey.USER_ID)
+        // .then((result) => {
+        //     alert("Message Sent, We will get back to you shortly", result.text);
+        // },
+        // (error) => {
+        //     alert("An error occurred, Please try again", error.text);
+        // });
+    }
+
+
     return (
         <div id='contact-me' className="contact-me">
-            
+            <div className="container">
+                <h3>Contact Me:</h3>
+                <form onSubmit={handleSubmit}>
+                    <input type='text' placeholder='Email' />
+                    <textarea placeholder='Message'></textarea>
+                    <button type='submit'>Send</button>
+                </form>
+            </div>
         </div>
     )
 }
